9de37697204a22f71cc20b4a5dfd6b7f62d8ae02
Mohammad Shariq <shariqsway@gmail.com>
2022-04-11 21:58:52 +0530

    Detection Location

--- weather/App.js
+++ weather/App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+  const { errorMessage, setErrorMessage } = useState(null);
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function load() {
+    try {
+      let { status } = await Location.requestBackgroundPermissionsAsync();
+      if (status != "granted") {
+        setErrorMessage("Cannot Access the location");
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    } catch (error) {}
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello from React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
